5c5f9a3d26528d9b0f38e531271ddd18fac2e2a4
R-emon <rabiulislamemon1@gmail.com>
2022-05-02 15:58:28 +0600

    Created admin_login_script.php and added connection with DB

---
+++ admin_login_script.php
@@ -0,0 +1,16 @@
+<?<php>
+
+    $servernanme= "localhost";
+    $dBUsername = "root";
+    $dBPassword = "";
+    $dBName = "nsu_shuttle";
+
+    $con = mysqli_connect($servernanme, $dBUsername, $dBPassword, $dBName);
+
+
+
+
+
+
+
+</php>
