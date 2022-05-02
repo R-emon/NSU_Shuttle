943f980494071551720049011cf387fac4608892
R-emon <rabiulislamemon1@gmail.com>
2022-05-02 16:19:22 +0600

    added sql query

--- admin_login_script.php
+++ admin_login_script.php
@@ -7,7 +7,16 @@
 
     $con = mysqli_connect($servernanme, $dBUsername, $dBPassword, $dBName);
 
+    $id = $_POST['id'];
+    $password = $_POST['password'];
 
+    if ($id == null) {
+        die('Invalid ID');
+    }
+    
+    $sql = "SELECT * FROM admin WHERE Admin_ID='$id' LIMIT 1";
+    $result = $con->query($sql);
+    $user = $result->fetch_assoc();
 
 
 
