9e1c756fefc99d654e7d108bef503c2aae345c17
SumaiyaMouri22 <sumaiya.mouri@northosouth.edu>
2022-05-07 01:49:58 +0600

    implemented booking_script.php

---
+++ booking_script.php
@@ -0,0 +1,27 @@
+<?php
+
+$db = mysqli_connect("localhost","root","","nsu_shuttle");
+
+if(!$db)
+{
+    die("Connection error: " .mysqli_connect_error());
+}
+
+    $id = $_POST['ID'];
+    $ts = $_POST['time_slot'];
+    $way = $_POST['Way'];
+    $date = $_POST['Date'];
+    $route = $_POST['route_id'];
+    
+$sql = "INSERT INTO bookings(User_ID, Route_ID, Way, Time_Slot, Date) VALUES ($id, '$route', '$way', '$ts', '$date')";
+
+if(mysqli_query($db,$sql))
+{ 
+   echo "Booking Confirmed";
+}
+    else
+  {
+    echo "There is an error: " . mysqli_error($db);
+  } 
+
+?>
