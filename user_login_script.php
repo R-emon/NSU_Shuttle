af49b65f9ec22f079b89280231fa77d63546d737
R-emon <rabiulislamemon1@gmail.com>
2022-05-06 17:29:22 +0600

    query to fetch login data

--- user_login_script.php
+++ user_login_script.php
@@ -9,4 +9,16 @@ $dBName = "nsu_shuttle";
 
 $con = mysqli_connect($servernanme, $dBUsername, $dBPassword, $dBName);
 
+$id = $_POST['id'];
+$password = $_POST['password'];
+
+if ($id == null) {
+    die('Invalid ID');
+}
+
+$sql = "SELECT * FROM user WHERE User_ID='$id' LIMIT 1";
+$result = $con->query($sql);
+
+$user = $result->fetch_assoc();
+
 ?>
