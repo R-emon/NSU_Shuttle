f4001d77d47b4ac0c794b2971842ff58d63467be
rkRashik <rkrashik@gmail.com>
2022-05-07 02:16:53 +0600

    implemented bus_remove.php

---
+++ bus_remove.php
@@ -0,0 +1,25 @@
+<!-- Author: Md. Redwan-ul-islam Rashik  -->
+
+<?php 
+
+$db = mysqli_connect("localhost","root","","nsu_shuttle");
+
+if(!$db)
+{
+    die("Connection error: " .mysqli_connect_error());
+}
+
+    $id = $_POST['driver_ID'];
+ 
+    $sql1 = "UPDATE bus SET Driver_ID = NULL WHERE Driver_ID = '$id'";
+    $sql2= "DELETE FROM driver WHERE driver_ID ='$id'";  
+ 
+if(mysqli_query($db,$sql1) && mysqli_query($db,$sql2))
+{ 
+   echo "Driver Removed.";
+}
+    else
+  {
+    echo "There is an error: " . mysqli_error($db);
+  } 
+?>
