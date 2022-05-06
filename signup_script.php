e213dc1faee3fddf4a107b58a5105b4bdd9acb1a
rkRashik <rkrashik@gmail.com>
2022-05-06 23:08:42 +0600

    implemented condition check for successfull signup

--- signup_script.php
+++ signup_script.php
@@ -14,4 +14,38 @@ $id = $_POST['id'];
 
     $con = mysqli_connect($servernanme, $dBUsername, $dBPassword, $dBName);
 
+    if(!$con){
+        die("Connection Failed: ".mysqli_connect_error());
+    }
+    if ($id == null) {
+       die("Invalid ID"); 
+    }
+    if ($name == null) {
+       die("Invalid name"); 
+    }
+    if ($phone == null) {
+       die("Invalid phone"); 
+    }
+    if ($id == null) {
+       die("Invalid ID"); 
+    }
+    if ($password == null) {
+       die("Invalid password"); 
+    }
+    if ($password != $password2) {
+        die("Password didn't match");
+    }
+
+    $sql = "INSERT INTO user (User_ID, Name, Phone, Address, Password)
+    VALUES ('$id', '$name', '$phone', '$address', '$password')";
+
+    if ($con->query($sql) === TRUE) {
+    echo "New user created! <br/>";
+    echo '<a href="http://localhost/NSU_Shuttle/userlogin.html">Login</a>';
+    } else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+    }
+
+    $con->close();
+
 ?>
