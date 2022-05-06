5a5e1ed1322e8f04abeb9967588f92ae7b2e391a
rkRashik <rkrashik@gmail.com>
2022-05-07 02:18:00 +0600

    driver_update.php implemented

---
+++ driver_update.php
@@ -0,0 +1,25 @@
+<!-- Author: Md. Redwan-ul-islam Rashik  -->
+
+<?php
+
+$db = mysqli_connect("localhost","root","","nsu_shuttle");
+
+if(!$db)
+{
+    die("Connection error: " .mysqli_connect_error());
+}
+
+    $bid = $_POST['bus_ID'];
+    $did = $_POST['driver_ID'];
+    
+$sql = "UPDATE driver SET Bus_ID = '$bid' WHERE Driver_ID = '$did'";
+
+if(mysqli_query($db,$sql))
+{ 
+   echo "Bus Updated";
+}
+    else
+  {
+    echo "There is an error: " . mysqli_error($db);
+  } 
+?>
