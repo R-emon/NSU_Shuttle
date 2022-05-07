9e061eb40f19cb2c48ece0f60359a5f000606097
R-emon <rabiulislamemon1@gmail.com>
2022-05-07 14:21:34 +0600

    route_remove.php added

---
+++ route_remove.php
@@ -0,0 +1,24 @@
+<!-- Author: Rabiul Islam Emon -->
+
+<?php 
+
+$db = mysqli_connect("localhost","root","","nsu_shuttle");
+
+if(!$db)
+{
+    die("Connection error: " .mysqli_connect_error());
+}
+
+    $id = $_POST['Route_ID'];
+ 
+    $sql = "DELETE FROM routes WHERE Route_ID= '$id'";  
+ 
+if(mysqli_query($db,$sql))
+{ 
+   echo "Route Removed.";
+}
+    else
+  {
+    echo "There is an error: " . mysqli_error($db);
+  } 
+?>
